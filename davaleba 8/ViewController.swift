1f58707f122241f5a4c2c6c3c7a05823cf45cdfa
giorgitabatadze <giotabatadze1998@gmail.com>
2020-04-24 19:07:16 +0400

    davaleba8

---
+++ davaleba 8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  davaleba 8
+//
+//  Created by gio tabatadze on 4/24/20.
+//  Copyright © 2020 gio tabatadze. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
